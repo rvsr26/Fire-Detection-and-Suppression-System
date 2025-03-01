fc47782c672b93cd578f3286b2ccf5244e37e620
Rajala Vishnu Sathwick <145008299+rvsr26@users.noreply.github.com>
2025-03-01 14:10:00 +0530

    feat: initialize LCD and output pins in setup

--- main.c
+++ main.c
@@ -21,8 +21,32 @@ typedef struct {
     Sensor gasSensor;
 } FireDetectionSystem;
 
+//LCD object
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
+// Global fire detection system object
+FireDetectionSystem fireDetectionSystem = {
+    .motor = {10, 11},
+    .ledRed = 12,
+    .ledGreen = 9,
+    .buzzer = 8,
+    .temperatureSensor = {A1, 0.0},
+    .gasSensor = {A0, 0.0}
+};
+
+void initFireSystem(FireDetectionSystem* system) {
+    pinMode(system->buzzer, OUTPUT);
+    pinMode(system->ledRed, OUTPUT);
+    pinMode(system->ledGreen, OUTPUT);
+    for (int i = 0; i < 2; i++) {
+        pinMode(system->motor[i], OUTPUT);
+    }
+}
+
 void setup() {
     Serial.begin(9600);
+    lcd.begin(16, 2);
+    initFireSystem(&fireDetectionSystem);
 }
 
 void loop() {
